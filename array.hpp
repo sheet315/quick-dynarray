81d4152b55bbbde2e2af2111e53a6c0e6d9222c5
sheet315 <shaundereuv@gmail.com>
2026-09-05 20:10:44 +0200

    made the constructor

--- array.hpp
+++ array.hpp
@@ -1,11 +1,29 @@
 #include <cstdint>
+#include <cstdlib>
+#include <bit>
+#include <memory>
 
 template <typename T>
 class dynArray {
     public:
-        size_t   elementCount;
-        size_t   elementWidth = sizeof(T);
+        size_t   size;
+        size_t   width;
 
-        uint8_t* arrPointer;
+        uint8_t* data;
 
+
+    dynArray(T v, size_t count) {
+        size = count;
+        width = sizeof(v);
+
+        size_t bytes = width * count;
+        size_t capacity = std::bit_ceil(bytes);
+
+
+        data = (uint8_t*)malloc(capacity);
+        T* arr = (T*)data;
+        for (size_t i = 0; i < count; i++) {
+            std::construct_at(&arr[i], v);
+        }
+    }
 };
